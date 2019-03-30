b84e2559bacd2a602fa38445ece154cf15f00d23
Monsets <monset008@gmail.com>
2019-03-30 17:03:04 +0300

    preprocessing step added

--- Cancer-detection/Nastnet/NasNET.py
+++ Cancer-detection/Nastnet/NasNET.py
@@ -10,10 +10,8 @@ from sklearn.model_selection import train_test_split
 from keras.callbacks import ModelCheckpoint
 from ..data.preprocessing import load_data
 
-TRAIN_SET_DATA = 'trainset-data.pic'
-TEST_SET_DATA = 'testset-data.pic'
-TRAIN_SET_LABELS = 'trainset-data.pic'
-TEST_SET_LABELS = 'testset-data.pic'
+DATA_PATH = 'trainset-data.pic'
+LABELS_PATH = 'trainset-data.pic'
 
 input_tensor = Input(shape=(32, 32, 3))
 
@@ -33,13 +31,13 @@ model = Model(inputs=model.input, outputs=predictions)
 # compile the model (should be done *after* setting layers to non-trainable)
 model.compile(optimizer='Adam', loss='categorical_crossentropy')
 
-x, y = load_data(TRAIN_SET_DATA, TRAIN_SET_LABELS)
+x, y = load_data(DATA_PATH, LABELS_PATH)
 y = to_categorical(y)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1)
 #data augmentation  using flips
 idg = ImageDataGenerator(horizontal_flip = True, vertical_flip = True)
 
 model.fit_generator(idg.flow(x_train, y_train, batch_size=64), steps_per_epoch = len(x_train) / 64, epochs = 100,
                     validation_data = idg.flow(x_test, y_test, batch_size = 64), validation_steps = len(x_test) / 64,
-                    callbacks = ModelCheckpoint('Checkpoints/nasnet-cancer-{val_loss:.2f}.hdf5', save_best_only = True))
+                    callbacks = ModelCheckpoint('checkpoints/nasnet-cancer-{val_loss:.2f}.hdf5', save_best_only = True))
--- Cancer-detection/data/preprocessing.py
+++ Cancer-detection/data/preprocessing.py
@@ -1,12 +1,48 @@
 import pickle
+import os
+import numpy as np
+import zipfile
+import pandas as p
+
+from PIL import Image
+from matplotlib.image import imsave
 
 
-def preprocess_data(data_path, save_path):
-    data = []
-    with open(save_path, 'wb') as f:
-        pickle.dump(data, f)
+def preprocess_data(d_path, l_path, zipfile_path = 'train.zip', extract_dir = 'data'):
+    '''get centered 32x32 patches of image and save in
+    pickle format'''
+    print('First launch. Data preprocessing started.')
+    zip_ref = zipfile.ZipFile(zipfile_path, 'r')
+    if not os.path.exists(extract_dir):
+        os.mkdir(extract_dir)
+    zip_ref.extractall(extract_dir)
+    zip_ref.close()
+
+    data_path = extract_dir
+    labels_db = p.read_csv('labels_csv', sep = ',')
+
+    all_images = []
+    all_labels = []
+    for _, _, files in os.walk(data_path):
+        for file in files:
+            img = Image.open(data_path + file)
+            img = np.array(img)
+            if img.shape[0] == 96:
+                img = img[32:64, 32:64, :]
+                all_images.append(img)
+                all_labels.append(labels_db['label'][np.where(labels_db['id'] == file)])
+
+    with open(d_path, 'wb') as f:
+        pickle.dump(all_images, f)
+    with open(l_path, 'wb') as f:
+        pickle.dump(all_labels, f)
+
 
 def load_data(data_path, labels_path = None):
+    '''fetch data from pickle format'''
+    if not os.path.exists(data_path):
+        preprocess_data(data_path, labels_path)
+
     with open(data_path, 'rb') as f:
         data = pickle.load(f)
 
@@ -16,3 +52,8 @@ def load_data(data_path, labels_path = None):
         return data, labels
 
     return data
+preprocess_data('./')
+
+
+
+
