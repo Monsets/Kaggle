c24424c29a46a4574ba85408928a0cfcbb7ca983
Monsets <monset008@gmail.com>
2019-03-30 20:25:01 +0300

    preprocessing step added

--- Cancer-detection/data/preprocessing.py
+++ Cancer-detection/data/preprocessing.py
@@ -7,36 +7,45 @@ import pandas as p
 from PIL import Image
 from matplotlib.image import imsave
 
-
-def preprocess_data(d_path, l_path, zipfile_path = 'train.zip', extract_dir = 'data'):
+def preprocess_data(d_path, l_path, zipfile_path = 'train.zip', extract_dir = 'imgs/'):
     '''get centered 32x32 patches of image and save in
     pickle format'''
     print('First launch. Data preprocessing started.')
-    zip_ref = zipfile.ZipFile(zipfile_path, 'r')
     if not os.path.exists(extract_dir):
         os.mkdir(extract_dir)
-    zip_ref.extractall(extract_dir)
-    zip_ref.close()
+
+        zip_ref = zipfile.ZipFile(zipfile_path, 'r')
+        zip_ref.extractall(extract_dir)
+        zip_ref.close()
+
+        zip_ref = zipfile.ZipFile('train_labels.csv.zip', 'r')
+        zip_ref.extractall(extract_dir)
+        zip_ref.close()
 
     data_path = extract_dir
-    labels_db = p.read_csv('labels_csv', sep = ',')
+    labels_db = p.read_csv('imgs/train_labels.csv', sep = ',', index_col = 'id')
 
     all_images = []
     all_labels = []
+    y = 0
     for _, _, files in os.walk(data_path):
         for file in files:
+            if file[-3:] != 'tif':
+                continue
             img = Image.open(data_path + file)
             img = np.array(img)
             if img.shape[0] == 96:
                 img = img[32:64, 32:64, :]
                 all_images.append(img)
-                all_labels.append(labels_db['label'][np.where(labels_db['id'] == file)])
+                all_labels.append(labels_db.loc[file[:-4], 'label'])
+                y += 1
+            if y % 10000 == 0:
+                print(y)
 
     with open(d_path, 'wb') as f:
         pickle.dump(all_images, f)
     with open(l_path, 'wb') as f:
         pickle.dump(all_labels, f)
-
 
 def load_data(data_path, labels_path = None):
     '''fetch data from pickle format'''
@@ -52,7 +61,6 @@ def load_data(data_path, labels_path = None):
         return data, labels
 
     return data
-preprocess_data('./')
 
 
 
